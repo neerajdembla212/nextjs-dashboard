106c1e816c7304a390a7f00f9515c653d6eed9cf
Neeraj Dembla <dembla.neeraj212@gmail.com>
2025-06-10 11:19:51 +0200

    creating dashboard with multiple components and sequential data fetching with delay

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,39 @@
-export default function dashboardPage() {
-    return <div>
-        <h1> Dashboard Page</h1>
-    </div>
+import { lusitana } from "@/app/ui/fonts";
+import { Card } from "@/app/ui/dashboard/cards";
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchCardData,
+} from "@/app/lib/data";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+
+export default async function dashboardPage() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+  console.log("revenue : ", revenue);
+  console.log("latestInvoices : ", latestInvoices);
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
